5557b1fd64202b92981f898fd69b657ef297c760
Duc Anh <ndaismeee@gmail.com>
2024-07-11 23:13:07 +0700

    done

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { WarehouseModule } from './modules/warehoouses/warehouse.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
-import { WarehouseEntity } from './enities/warehouse.entity';
+import { WarehouseEntity } from './entities/warehouse.entity';
 import { dbdatasource } from './database/data-source';
 
 
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,4 +1,4 @@
-import { WarehouseEntity } from 'src/enities/warehouse.entity';
+import { WarehouseEntity } from 'src/entities/warehouse.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dbdatasource: DataSourceOptions = {
@@ -7,18 +7,17 @@ export const dbdatasource: DataSourceOptions = {
     host: 'localhost',
     port: 3306,
     username: 'root',
-    password: 'root',
+    password: '',
     // Database name
     database: 'delivery-db',
     // Synchronize database schema with entities 
     synchronize: false,
     // TypeORM Entity
-    entities: ['src/enities/*{.js,.ts}'],
+    entities: ['src/entities/*{.js,.ts}'],
     // Your Migration path
-    migrations: ['src/migrations/*{.js, .ts}'],
+    migrations: ['src/migrations/*{.js,.ts}'],
     logging: true,
   };
   
 const dataSource = new DataSource(dbdatasource)
 export default dataSource
-//sdfkja;
